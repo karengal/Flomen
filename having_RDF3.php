5fdc1079ed5c937cc9e39ec1c362b5b64f55f1c7
Karen Gal <karengal@gmail.com>
2018-05-01 11:35:56 +0300

    changed having_RDF3

--- having_RDF3.php
+++ having_RDF3.php
@@ -1,6 +1,9 @@
 <html>
 <head>
 <title>Purchase RDF</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">      
+    <title>Login</title>
 </head>
 
 <BODY style="background-color: rgb(230, 230, 185)" aLink=green background= "1.jpg" vLink=blue>
@@ -11,16 +14,29 @@ Downloading RDF the free eddition, first version:
 <hr>
 
 <form align="center" action="having_RDF7.php" method="post" enctype="multipart/form-data" target="_parent"> 
+   
+    <div class="form-group row" align="center">
+         <label for="userLabel" class="col-form-label">User Name</label>
+        <input type=text  name="UserName" class="form-control" id="inputUserNameId" emptyerrormessage="Name cannot be empty"
+        style="width: 200px"> 
+    </div>
 
-User name: 
+    <div class="form-group row" align="center">
+         <label for="PasswordLabel" class="col-form-label">Password</label>
+        <input type=password class="form-control" id="inputPasswordId" emptyerrormessage="Password cannot be empty"
+        style="width: 200px"> 
+    </div>
+
+    <input type="submit" name="submit" class="btn" value="Submit" />
+<!-- User name: 
 <input type="text" name="UserName" align="center"/> <br />
 
 Password:
-<input type=PASSWORD name="Password1" align="center"/> <br />
+<input type=PASSWORD name="Password1" align="center"/> <br /> -->
 
 
 
-<input type="submit" name="submit" value="Submit" />
+
 </form> 
 
 
